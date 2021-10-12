8d3d3a726ee266b925d0f127fa27e4bca9112f9e
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-12 15:55:55 -0400

    changed settings file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,25 +75,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 #DATABASES = {
  #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql',
-   #     'NAME': 'postgres',
-	#'USER': 'postgres',
-	#'PASSWORD':'welcome1',
-	#'HOST':'127.0.0.1',
-    #    'HOST': 'postgres',
-	#'PORT':'5432',
-  #  }
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 #}
+
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+	'USER': 'postgres',
+	'PASSWORD':'welcome1',
+	'HOST':'127.0.0.1',
+      #  'HOST': 'postgres',
+	'PORT':'5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
