83c3ebc8c4a7c090600cabd3394f61f0bd08767e
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-16 15:25:10 -0400

    ursl file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('polls/',include('polls.urls')),
-    path('mortagage/',include('mortagage.urls')),
-    path('admin/', admin.site.urls),    
+    #path('polls/',include('polls.urls')),
+    #path('mortagage/',include('mortagage.urls')),
+    #path('admin/', admin.site.urls), 
+    path('polls/polls',include('polls.urls')),
+    path('mortagage/mortagage',include('mortagage.urls')),
+    path('admin/admin', admin.site.urls),    
+]   
 ]
