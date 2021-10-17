a6303fdd3f6fcbee5b50b20e1be24c164fa9f622
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-16 21:10:36 -0400

    urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
-urlpatterns = [
-    path('polls/',include('polls.urls')),
-    path('mortagage/',include('mortagage.urls')),
-    path('admin/', admin.site.urls), 
+#urlpatterns = [
+#    path('polls/',include('polls.urls')),
+ #   path('mortagage/',include('mortagage.urls')),
+#    path('admin/', admin.site.urls),
     
+#]
+
+urlpatterns = [
+    path('/', include('polls.urls')),
+    path('/', include('mortagage.urls')),
+    path('/', admin.site.urls),
+
 ]
