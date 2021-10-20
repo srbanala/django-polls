72669c3bab65f24f150b9f9cdf36912482b6d965
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-20 09:41:54 -0400

    deploy.sh file

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,2 +1,2 @@
 kubectl apply -f k8s
-kubectl set image deployments/polls-deployment polls-container=anreddy/django-polls:$GIT-SHA
+kubectl set image deployments/polls-deployment polls-container=anreddy/django-polls:$SHA
