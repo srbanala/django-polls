ace6e0ccbc31778b2e0743e7b9cc0d67837f2386
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-20 20:43:41 -0400

    deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,2 +1,3 @@
 kubectl apply -f k8s
 kubectl set image deployments/polls-deployment polls-container=anreddy/django-polls:$SHA
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/aws/deploy.yaml
